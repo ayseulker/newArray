1485ffb5950bda1417b1ae1416965d6d1c2f59f7
ayseulker <aysenrulkerr@gmail.com>
2023-04-12 00:20:33 +0300

    #

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 
-let array1 = ["Zeki", "Mehmet", "Fatma", "Halil", "Ahsen", "Zeynep", "Burak"]
-let array2 = ["Halil", "Veli", "Kerim", "Zeki", "Neriman", "Fatma", "Semih"]
+let array1 = ["Zeki", "Mehmet", "Fatma", "Halil", "Ahsen", "Zeynep", "Burak"];
+let array2 = ["Halil", "Veli", "Kerim", "Zeki", "Neriman", "Fatma", "Semih"];
 
 
-let newArray=[]
+let newArray=[];
 
 
 for(array of array1){
     
-   newArray.push(array)
+   newArray.push(array);
 }
 
 for(arrays of array2){
     if(newArray.includes(arrays)){
         newArray;
     }else{
-        newArray.push(arrays)
+        newArray.push(arrays);
     }
 }
 
-newArray = newArray.sort()
+newArray = newArray.sort();
    
-console.log(newArray)
+console.log(newArray);
 
 
